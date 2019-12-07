ae92a7d81a0e0d3d74113c3f464d0d1426b6e630
Martin Cabala <cabala.mato@gmail.com>
2019-12-07 10:21:49 +0100

    chore: initial project setup

---
+++ src/main/java/com/bsc/postalservice/Main.java
@@ -0,0 +1,8 @@
+package com.bsc.postalservice;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi, this is BSC postal delivery service");
+    }
+}
