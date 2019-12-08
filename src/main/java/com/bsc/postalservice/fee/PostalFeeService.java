a2c0417b159add4ade198eb21945d28f4aec75cd
Martin Cabala <cabala.mato@gmail.com>
2019-12-08 12:15:55 +0100

    feat: introduce fee service which returns fee based on package weight

---
+++ src/main/java/com/bsc/postalservice/fee/PostalFeeService.java
@@ -0,0 +1,35 @@
+package com.bsc.postalservice.fee;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+public class PostalFeeService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(PostalFeeService.class);
+
+  private Map<Float, Float> weightFeeStructure;
+
+  public PostalFeeService() {
+    weightFeeStructure = new TreeMap<>();
+  }
+
+  public void addFeeEntry(Float weight, Float fee) {
+    if(weightFeeStructure.get(weight) != null) {
+      LOG.warn("Fee structure already contains weight: " + weight + " Overriding previous fee.");
+    }
+    weightFeeStructure.put(weight, fee);
+  }
+
+  public Float getFeeBasedOnWeight(Float packageWeight) {
+    // treemap's keys are in ascending natural order
+    List<Float> weights = new ArrayList<> (weightFeeStructure.keySet());
+    Collections.reverse(weights);
+    return weights.stream()
+        .filter(weight -> packageWeight >=weight)
+        .findFirst()
+        .map(weightFeeStructure::get)
+        .orElse(0.0f);
+  }
+}
---
+++ src/test/java/com/bsc/postalservice/fee/PostalFeeServiceTest.java
@@ -0,0 +1,51 @@
+package com.bsc.postalservice.fee;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class PostalFeeServiceTest {
+
+  private PostalFeeService feeService;
+
+  @Before
+  public void init() {
+    /*
+      Fee structure looks like this, for testing purposes it is initialized in arbitrary order
+
+      WEIGHT | FEE
+      -------------
+      10       5.00
+      5        2.50
+      3        2.00
+      2        1.50
+      1        1.00
+      0.5      0.70
+      0.2      0.50
+     */
+    feeService = new PostalFeeService();
+    feeService.addFeeEntry(0.5f, 0.70f);
+    feeService.addFeeEntry(2f, 1.50f);
+    feeService.addFeeEntry(0.2f, 0.50f);
+    feeService.addFeeEntry(1f, 1.00f);
+    feeService.addFeeEntry(5f, 2.50f);
+    feeService.addFeeEntry(3f, 2.00f);
+    feeService.addFeeEntry(10f, 5.00f);
+  }
+
+  @Test
+  public void getFeeBasedOnWeight() {
+    assertThat(feeService.getFeeBasedOnWeight(12.0f), is(5.0f));
+    assertThat(feeService.getFeeBasedOnWeight(10.0f), is(5.0f));
+    assertThat(feeService.getFeeBasedOnWeight(0.10f), is(0.0f));
+  }
+
+  @Test
+  public void thatOverrideBySameWeightWorks() {
+    feeService.addFeeEntry(10f, 5.5f);
+    assertThat(feeService.getFeeBasedOnWeight(12.0f), is(5.5f));
+  }
+
+}
