2f66980b6c67d780513d3eec744b51bc8ddbdca1
Martin Cabala <cabala.mato@gmail.com>
2019-12-10 18:06:46 +0100

    test: add one more scheduling test for real job

--- src/test/java/com/bsc/postalservice/schedule/FixedPeriodJobSchedulerTest.java
+++ src/test/java/com/bsc/postalservice/schedule/FixedPeriodJobSchedulerTest.java
@@ -7,8 +7,10 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
@@ -43,6 +45,16 @@ public class FixedPeriodJobSchedulerTest {
     jobScheduler.shutDown();
 
     verify(executorService, times(1)).shutdown();
+  }
+
+  @Test
+  public void thatSchedulingActuallyWork() throws InterruptedException {
+    AtomicReference<Boolean> jobRan = new AtomicReference<>(false);
+    FixedPeriodJobScheduler realScheduler = new FixedPeriodJobScheduler();
+    realScheduler.scheduleJob(() -> jobRan.set(true), new FixedPeriodJobDetail(1, 10, SECONDS));
+
+    Thread.sleep(1500);
+    assertTrue(jobRan.get());
   }
 
   private Job emptyJob() {
